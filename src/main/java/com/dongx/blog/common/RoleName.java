34e04b5d3cd2d87a46040ffed4a96241299b8e02
Dongx <775422441@qq.com>
2018-06-01 19:14:22 +0800

    角色名称常量类

---
+++ src/main/java/com/dongx/blog/common/RoleName.java
@@ -0,0 +1,16 @@
+package com.dongx.blog.common;
+
+/**
+ * RoleName
+ *
+ * @author: dongx
+ * Description: 角色名称常量类
+ * Created in: 2018-06-01 14:15
+ * Modified by:
+ */
+public class RoleName {
+	
+	public static final String ROLE_ADMIN = "ROLE_AMDIN";
+	
+	public static final String ROLE_USER = "ROLE_USER";
+}
