00322bf2bcf5ce872430177fd7b288c0fb887d07
Dongx <775422441@qq.com>
2018-06-07 09:14:20 +0800

    增加删除保存方法

--- src/main/java/com/dongx/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/dongx/blog/service/impl/CommentServiceImpl.java
@@ -1,13 +1,21 @@
 package com.dongx.blog.service.impl;
 
 import com.dongx.blog.common.CommonStatus;
+import com.dongx.blog.dto.CommentDTO;
 import com.dongx.blog.entity.Comment;
 import com.dongx.blog.resposity.CommentRepository;
 import com.dongx.blog.service.CommentService;
 import com.dongx.blog.sys.ServerResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Server;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 
 /**
@@ -18,23 +26,63 @@ import java.util.List;
  * Created in: 2018-06-04 16:17
  * Modified by:
  */
+@Slf4j
+@Service
 public class CommentServiceImpl implements CommentService {
 	
 	@Resource
 	CommentRepository commentRepository;
 	
-	public ServerResponse findAll(String blogId) {
+	public ServerResponse saveByBlogId(CommentDTO commentDTO) {
+		if (commentDTO == null) {
+			return null;
+		}
+		
+		Comment comment = new Comment();
+		BeanUtils.copyProperties(commentDTO, comment);
+		comment.setCrreateTime(Date.from(Instant.now()));
+		Comment result = commentRepository.save(comment);
+		
+		if (result != null) {
+			log.info("评论保存成功: {}", comment.getContent());
+			return ServerResponse.createBySuccess("评论保存成功", comment);
+		}
+		log.info("评论保存失败: {}", commentDTO.getBlogId());
+		return ServerResponse.createByError("评论保存失败");
+	}
+	
+	
+	public ServerResponse findAllByBlogId(String blogId) {
 		
 		if (StringUtils.isEmpty(blogId)) {
 			return null;	
 		}
 
 		List<Comment> result = commentRepository.findAllByBlogIdAndStatus(blogId, CommonStatus.ACTIVE.getCode());
-		
 		if (result != null) {
 			return ServerResponse.createBySuccess(result);
 		}
 		
 		return ServerResponse.createByError();
+	}
+	
+	public ServerResponse deleteByCommentId(CommentDTO commentDTO) {
+		
+		if (commentDTO == null) {
+			return null;
+		}
+
+		Comment comment = new Comment();
+		BeanUtils.copyProperties(commentDTO, comment);
+		comment.setStatus(CommonStatus.UNACTIVE.getCode());
+		Comment result = commentRepository.save(comment);
+		
+		if (result != null) {
+			log.info("评论删除成功: {}", comment.getId());
+			return ServerResponse.createBySuccess("评论删除成功");
+		}
+		
+		log.info("评论删除失败: {}", comment.getId());
+		return ServerResponse.createByError("评论删除失败");
 	}
 }
